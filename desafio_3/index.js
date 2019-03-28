04363e301cce61fecff268b9dc45550bcb492aa0
Douglas <dldouglaslopes@gmail.com>
2019-03-28 12:12:07 -0300

    Comentário adicionado em desafio_3

--- desafio_3/index.js
+++ desafio_3/index.js
@@ -3,5 +3,6 @@ const axios = require('axios');
 
 axios.get(process.argv[2])
   .then(function (response) {
+  	//referente ao numero de issues abertass
     console.log(response.data.open_issues_count + " issue(s)");
   });
